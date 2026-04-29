83c709f5a73d2c029d8a9a39ab895812a20d4fc1
Sanagala Radhika <boyaradhika9@gmail.com>
2026-04-29 16:52:38 +0530

    Initialize Express server with routes and middleware

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const sequelize = require('./config/db');
+
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+sequelize.sync().then(() => {
+  app.listen(5000, () => console.log('Server running'));
+});
